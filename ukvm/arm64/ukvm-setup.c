b1be5ccbd935b13df6e7e991e4728700a2a99484
Wei Chen <Wei.Chen@arm.com>
2017-03-22 03:18:19 +0000

    ukvm/arm64: Implement setup_boot_info for ARM64
    The memory map of ARM64 is different from x86_64, so we have to implement
    an ARM64 version setup_boot_info.
    
    Signed-off-by: Wei Chen <Wei.Chen@arm.com>

--- ukvm/arm64/ukvm-setup.c
+++ ukvm/arm64/ukvm-setup.c
@@ -51,3 +51,30 @@
 #define BOOT_UNUSED     0x10000
 #define BOOT_UNUSED_SZ  (BOOT_ELF_ENTRY - BOOT_UNUSED)
 #define BOOT_ELF_ENTRY  0x100000
+
+void setup_boot_info(uint8_t *mem,
+                    uint64_t size,
+                    uint64_t kernel_end,
+                    int argc, char **argv)
+{
+    struct ukvm_boot_info *bi = (struct ukvm_boot_info *)(mem + BOOT_INFO);
+    uint64_t cmdline = BOOT_INFO + sizeof(struct ukvm_boot_info);
+    size_t cmdline_free = BOOT_UNUSED - cmdline - 1;
+    char *cmdline_p = (char *)(mem + cmdline);
+
+    bi->mem_size = size;
+    bi->kernel_end = kernel_end;
+    bi->cmdline = cmdline;
+    cmdline_p[0] = 0;
+
+    for (; *argv; argc--, argv++) {
+        size_t alen = snprintf(cmdline_p, cmdline_free, "%s%s", *argv,
+                (argc > 1) ? " " : "");
+        if (alen >= cmdline_free) {
+            warnx("command line too long, truncated");
+            break;
+        }
+        cmdline_free -= alen;
+        cmdline_p += alen;
+    }
+}
