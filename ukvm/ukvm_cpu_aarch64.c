f04788a5aeb2b6c867a9ecd23dd25708653495e6
Wei Chen <Wei.Chen@arm.com>
2017-07-07 10:19:07 +0000

    ukvm: Enable aarch64 guest MMU
    If we do not set up the guest MMU then the guest and host will
    access memory with a different cacheability which can lead to
    data corruption. We need to setup the guest to use the same
    cacheability as the host by setting up the MMU within the guest.
    
    Hence the guest will use virtual address to access memory. But guest
    on other architecture are using the physical address. We don’t want
    to make guest on aarch64 be special, so we create page tables for
    guest to do 1:1 mapping between virtual address and intermediate
    physical address.

---
+++ ukvm/ukvm_cpu_aarch64.c
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2015-2017 Contributors as noted in the AUTHORS file
+ *
+ * This file is part of ukvm, a unikernel monitor.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * ukvm_cpu_aarch64.c: Common architecture-dependent code supporting aarch64
+ * backend implementations.
+ */
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+
+#include <linux/kvm.h>
+
+#include "ukvm.h"
+#include "ukvm_hv_kvm.h"
+#include "ukvm_cpu_aarch64.h"
+
+static int s_page_used_for_pgt;
+static uint64_t alloc_page_for_pgt(void)
+{
+    uint64_t phy_addr;
+
+    phy_addr = AARCH64_PAGE_TABLE + PAGE_SIZE * s_page_used_for_pgt;
+    if (phy_addr >= AARCH64_GUEST_MIN_BASE)
+        err(1, "Run out of the memory for page table!");
+
+    s_page_used_for_pgt++;
+
+    return phy_addr;
+}
+
+static void init_pte_table(struct pte *pte_tbl, uint64_t start, uint64_t size,
+                          uint64_t code_area_entry, uint64_t code_area_end)
+{
+    int idx, pte_entries;
+
+    pte_entries = DIV_ROUND_UP(size, PAGE_SIZE);
+    if (!pte_entries)
+        err(1, "Zero size is not permit!");
+
+    for (idx = 0; idx < pte_entries; idx++)
+    {
+        uint64_t prot = PROT_NORMAL;
+        uint64_t out_address = start + PAGE_SIZE * idx;
+        uint64_t *pte_entry = pte_tbl->entry + idx;
+
+        /*
+         * If we are still in core area, we have to check the region list
+         * to set correct attributes.
+         */
+        if (out_address >= code_area_entry &&
+            out_address < ukvm_end_of_kernel_rodata) {
+            /* Fill the pte for readonly aread */
+            prot |= PTE_RDONLY;
+
+            /* Fill the pte for text aread */
+            if (out_address < ukvm_end_of_kernel_etext)
+                prot &= ~PTE_PXN; /* we need the exec in EL1 */
+        }
+
+        *pte_entry = out_address | prot;
+    }
+}
+
+static void init_pmd_table(uint8_t *va_addr, struct pmd *pmd_tbl,
+                           uint64_t start, uint64_t size,
+                           uint64_t code_area_entry,
+                           uint64_t code_area_end)
+{
+    int idx, pmd_entries;
+    uint64_t out_address, left_sz;
+    uint64_t *pmd_entry;
+
+    pmd_entries = DIV_ROUND_UP(size, PMD_SIZE);
+    if (!pmd_entries)
+        err(1, "Zero size is not permit!");
+
+    for (idx = 0; idx < pmd_entries; idx++)
+    {
+        pmd_entry = pmd_tbl->entry + idx;
+        out_address = start + PMD_SIZE * idx;
+
+        /* Caculate the left memory size that this pmd will cover */
+        left_sz = size - out_address;
+        if (left_sz > PMD_SIZE)
+            left_sz = PMD_SIZE;
+
+        /*
+         * 1. If this memory is in code area, we have to create a pte table.
+         * 2. If the size is smaller than PMD_SIZE, the last pmd entry must
+         *    be configured as table attribute.
+         */
+        if ((out_address < code_area_end) ||
+            (idx == (pmd_entries - 1)  && (left_sz < PMD_SIZE))) {
+            uint64_t pte_tbl = alloc_page_for_pgt();
+
+            init_pte_table((struct pte *)(va_addr + pte_tbl), out_address,
+                           left_sz, code_area_entry, code_area_end);
+            *pmd_entry = pte_tbl | PMD_TYPE_TABLE;
+        }
+        /* Use 2MB block to map VA <--> PA for other memory area */
+        else
+            *pmd_entry = out_address | PROT_SECT_NORMAL;
+    }
+}
+
+static void init_pud_table(uint8_t *va_addr, struct pud *pud_tbl,
+                           uint64_t start, uint64_t size,
+                           uint64_t code_area_entry,
+                           uint64_t code_area_end)
+{
+    int idx, pud_entries;
+    uint64_t left_sz, out_address;
+    uint64_t *pud_entry;
+
+    /* How many pud we should use */
+    pud_entries = DIV_ROUND_UP(size, PUD_SIZE);
+    if (pud_entries == 0)
+        err(1, "Zero size for guest memory is not permitted!");
+    if (pud_entries > 512)
+        err(1, "Currently, we only support 512GB address space!");
+
+    /*
+     * Because the we placed the code at the beginning of guest memroy,
+     * and this memory has been split to multiple sections. Different
+     * sections may have different attributes, so we have to use page to
+     * manage these sections' attribute. For other memory, we can use
+     * block to do VA-PA mapping.
+     */
+    for (idx = 0; idx < pud_entries; idx++) {
+        pud_entry = pud_tbl->entry + idx;
+        out_address = start + PUD_SIZE * idx;
+
+        /* Caculate the left memory size that this pud will cover */
+        left_sz = size - out_address;
+        if (left_sz > PUD_SIZE)
+            left_sz = PUD_SIZE;
+
+        /*
+         * 1. If this memory is in code area, we have to create a pud table.
+         * 2. If the size is smaller than PUD_SIZE, the last pud entry must
+         *    be configured as table attribute.
+         */
+        if ((out_address < code_area_end) ||
+            (idx == (pud_entries - 1) && (left_sz < PUD_SIZE))) {
+            /* Create next level for code area */
+            uint64_t pmd_tbl = alloc_page_for_pgt();
+
+            init_pmd_table(va_addr, (struct pmd *)(va_addr + pmd_tbl),
+                           out_address, left_sz,
+                           code_area_entry, code_area_end);
+
+            /* Update pmd table address to pud entry */
+            *pud_entry = pmd_tbl | PUD_TYPE_TABLE;
+        }
+        /* Other memory can use 1GB mapping */
+        else
+            *pud_entry = out_address | PROT_SECT_NORMAL;
+    }
+}
+
+/* We put the page table at the unused memory */
+static void aarch64_setup_pagetables(uint8_t *va_addr, uint64_t size,
+                                     uint64_t guest_phys_addr,
+                                     uint64_t code_area_entry,
+                                     uint64_t code_area_end)
+{
+    struct pgd *pgd;
+    uint64_t pud_tbl;
+
+    /* First page in AARCH64_PAGE_TABLE is used for pgd */
+    pgd = (struct pgd *)(va_addr + alloc_page_for_pgt());
+
+    /*
+     * 1 pud table can provide 512GB address space, so set 1 pud in pgd
+     * table is enough. We use the second page for pud table.
+     */
+    pud_tbl = alloc_page_for_pgt();
+
+    init_pud_table(va_addr, (struct pud *)(va_addr + pud_tbl),
+                   0, size, code_area_entry, code_area_end);
+
+    pgd->entry[0] = pud_tbl | PGD_TYPE_TABLE;
+}
+
+static int aarch64_set_one_register(int vcpufd, uint64_t id, uint64_t data)
+{
+    struct kvm_one_reg one_reg = {
+        .id   = id,
+        .addr = (uint64_t)&data,
+    };
+
+    return ioctl(vcpufd, KVM_SET_ONE_REG, &one_reg);
+}
+
+static int aarch64_get_one_register(int vcpufd, uint64_t id, uint64_t *pdata)
+{
+    struct kvm_one_reg one_reg = {
+        .id   = id,
+        .addr = (uint64_t)pdata,
+    };
+
+    return ioctl(vcpufd, KVM_GET_ONE_REG, &one_reg);
+}
+
+static void aarch64_enable_guest_mmu(int vcpufd)
+{
+    int ret;
+    uint64_t data;
+
+    /*
+     * Setup Memory Attribute Indirection Register EL1, this register
+     * must be set before setup page tables.
+     */
+    data = MAIR_EL1_INIT;
+    ret = aarch64_set_one_register(vcpufd, MAIR_EL1, data);
+    if (ret == -1)
+         err(1, "KVM: Setup Memory Attribute Indirection Register failed");
+
+    /* Setup Translation Control Register EL1 */
+    data = TCR_EL1_INIT;
+    ret = aarch64_set_one_register(vcpufd, TCR_EL1, data);
+    if (ret == -1)
+         err(1, "KVM: Setup Translation Control Register EL1 failed");
+
+    /* Setup Translation Table Base Register 0 EL1 */
+    data = AARCH64_PAGE_TABLE;
+    ret = aarch64_set_one_register(vcpufd, TTBR0_EL1, data);
+    if (ret == -1)
+         err(1, "KVM: Translation Table Base Register 0 EL1 failed");
+
+    /* Setup Translation Table Base Register 1 EL1 */
+    data = AARCH64_PAGE_TABLE;
+    ret = aarch64_set_one_register(vcpufd, TTBR1_EL1, data);
+    if (ret == -1)
+         err(1, "KVM: Setup Translation Table Base Register 1 EL1 failed");
+
+    /* Setup System Control Register EL1 to enable MMU */
+    ret = aarch64_get_one_register(vcpufd, SCTLR_EL1, &data);
+    if (ret == -1)
+         err(1, "KVM: Get System Control Register EL1 failed");
+
+    /* Enable MMU and I/D Cache for EL1 */
+    data |= (_SCTLR_M | _SCTLR_C | _SCTLR_I);
+    ret = aarch64_set_one_register(vcpufd, SCTLR_EL1, data);
+    if (ret == -1)
+         err(1, "KVM: Setup System Control Register EL1 failed");
+}
---
+++ ukvm/ukvm_cpu_aarch64.h
@@ -0,0 +1,283 @@
+/*
+ * Copyright (c) 2015-2017 Contributors as noted in the AUTHORS file
+ *
+ * This file is part of ukvm, a unikernel monitor.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * ukvm_cpu_aarch64.h: CPU constants and initialisation data common to aarch64
+ * backend implementations.
+ */
+
+#ifndef UKVM_CPU_AARCH64_H
+#define UKVM_CPU_AARCH64_H
+
+#ifndef _BITUL
+
+#ifdef __ASSEMBLY__
+#define _AC(X,Y)                X
+#define _AT(T,X)                X
+#else
+#define __AC(X,Y)               (X##Y)
+#define _AC(X,Y)                __AC(X,Y)
+#define _AT(T,X)                ((T)(X))
+#endif
+
+#define _BITUL(x)               (_AC(1,UL) << (x))
+#define _BITULL(x)              (_AC(1,ULL) << (x))
+
+#endif
+
+#define BITS_32     32
+#define BITS_64     64
+
+#define PAGE_SHIFT  12
+#define PAGE_SIZE   (1 << (PAGE_SHIFT))
+
+/*
+ * Memory map:
+ *
+ * 0x100000    loaded elf file (linker script dictates location)
+ * ########    unused
+ * 0x010000    memory for page table
+ * ########    command line arguments
+ * 0x002000    ukvm_boot_info
+ * 0x001000    non-cacheable page
+ * 0x000000    MMIO space to emulate IO abort
+ */
+#define AARCH64_GUEST_MIN_BASE  0x100000
+#define AARCH64_PAGE_TABLE      0x10000
+#define AARCH64_CMDLINE_BASE    0xC000
+#define AARCH64_CMDLINE_SZ      (AARCH64_PAGE_TABLE - AARCH64_CMDLINE_BASE)
+#define AARCH64_BOOT_INFO       0x1000
+#define AARCH64_BOOT_INFO_SZ    (AARCH64_CMDLINE_BASE - AARCH64_BOOT_INFO)
+#define AARCH64_MMIO_BASE       0x0
+#define AARCH64_MMIO_SZ         (AARCH64_BOOT_INFO - AARCH64_MMIO_BASE)
+
+#define GENMASK32(h, l) \
+    (((~0U) << (l)) & (~0U >> (BITS_32 - 1 - (h))))
+#define GENMASK64(h, l) \
+    (((~0UL) << (l)) & (~0UL >> (BITS_64 - 1 - (h))))
+
+/*
+ * KVM/ARM64 provides an interface to userspace to modify the
+ * VM registers. This interface describe the register by index.
+ * We have to define the index here for those registers that we
+ * will modify.
+ */
+
+/* Normal registers are mapped as coprocessor 16. */
+#define KVM_REG_ARM_CORE    (0x0010 << KVM_REG_ARM_COPROC_SHIFT)
+
+#define ARM64_CORE_REG(x)   \
+            (KVM_REG_ARM64 | KVM_REG_SIZE_U64 | \
+            KVM_REG_ARM_CORE | KVM_REG_ARM_CORE_REG(x))
+
+/* Memory Attribute Indirection Register EL1 */
+#define MAIR_EL1            ARM64_SYS_REG(3, 0, 10, 2, 0)
+
+/* Memory types available. */
+#define MT_DEVICE_nGnRnE    0
+#define MT_DEVICE_nGnRE     1
+#define MT_DEVICE_GRE       2
+#define MT_NORMAL_NC        3
+#define MT_NORMAL           4
+#define MT_NORMAL_WT        5
+
+#define MAIR(attr, mt)      (_AC(attr, UL) << ((mt) * 8))
+
+#define MAIR_EL1_INIT       \
+        MAIR(0x00, MT_DEVICE_nGnRnE) | MAIR(0x04, MT_DEVICE_nGnRE) | \
+        MAIR(0x0C, MT_DEVICE_GRE) | MAIR(0x44, MT_NORMAL_NC) | \
+        MAIR(0xFF, MT_NORMAL) | MAIR(0xBB, MT_NORMAL_WT)
+
+/* Translation Control Register EL1 */
+#define TCR_EL1             ARM64_SYS_REG(3, 0, 2, 0, 2)
+
+/*
+ * TCR flags.
+ */
+#define TCR_T0SZ_OFFSET     0
+#define TCR_T1SZ_OFFSET     16
+#define TCR_T0SZ(x)         ((_AC(64, UL) - (x)) << TCR_T0SZ_OFFSET)
+#define TCR_T1SZ(x)         ((_AC(64, UL) - (x)) << TCR_T1SZ_OFFSET)
+#define TCR_TxSZ(x)         (TCR_T0SZ(x) | TCR_T1SZ(x))
+
+#define TCR_IRGN0_SHIFT     8
+#define TCR_IRGN0_WBWA      (_AC(1, UL) << TCR_IRGN0_SHIFT)
+#define TCR_IRGN1_SHIFT     24
+#define TCR_IRGN1_WBWA      (_AC(1, UL) << TCR_IRGN1_SHIFT)
+#define TCR_IRGN_WBWA       (TCR_IRGN0_WBWA | TCR_IRGN1_WBWA)
+
+#define TCR_ORGN0_SHIFT     10
+#define TCR_ORGN0_WBWA      (_AC(1, UL) << TCR_ORGN0_SHIFT)
+#define TCR_ORGN1_SHIFT     26
+#define TCR_ORGN1_WBWA      (_AC(1, UL) << TCR_ORGN1_SHIFT)
+#define TCR_ORGN_WBWA       (TCR_ORGN0_WBWA | TCR_ORGN1_WBWA)
+
+#define TCR_SH0_SHIFT       12
+#define TCR_SH0_INNER       (_AC(3, UL) << TCR_SH0_SHIFT)
+#define TCR_SH1_SHIFT       28
+#define TCR_SH1_INNER       (_AC(3, UL) << TCR_SH1_SHIFT)
+#define TCR_SHARED          (TCR_SH0_INNER | TCR_SH1_INNER)
+
+#define TCR_TG0_SHIFT       14
+#define TCR_TG0_4K          (_AC(0, UL) << TCR_TG0_SHIFT)
+#define TCR_TG1_SHIFT       30
+#define TCR_TG1_4K          (_AC(2, UL) << TCR_TG1_SHIFT)
+
+#define TCR_ASID16          (_AC(1, UL) << 36)
+#define TCR_TBI0            (_AC(1, UL) << 37)
+
+#define TCR_TG_FLAGS        TCR_TG0_4K | TCR_TG1_4K
+#define TCR_CACHE_FLAGS     TCR_IRGN_WBWA | TCR_ORGN_WBWA
+
+#define TCR_EL1_INIT        \
+            TCR_TxSZ(48) | TCR_CACHE_FLAGS | TCR_SHARED | \
+			TCR_TG_FLAGS | TCR_ASID16 | TCR_TBI0;
+
+/* Translation Table Base Register 0 EL1 */
+#define TTBR0_EL1           ARM64_SYS_REG(3, 0, 2, 0, 0)
+
+/* Translation Table Base Register 1 EL1 */
+#define TTBR1_EL1           ARM64_SYS_REG(3, 0, 2, 0, 1)
+
+/* System Control Register EL1 */
+#define SCTLR_EL1           ARM64_SYS_REG(3, 0, 1, 0, 0)
+#define _SCTLR_M            _BITUL(0)
+#define _SCTLR_C            _BITUL(2)
+#define _SCTLR_I            _BITUL(12)
+
+/* Definitions of Page tables */
+#define PUD_SIZE    0x40000000
+#define PUD_MASK	(~(PUD_SIZE-1))
+#define PMD_SIZE    0x00200000
+#define PMD_MASK	(~(PMD_SIZE-1))
+
+#define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
+struct pte {
+    uint64_t entry[1];
+};
+
+struct pmd {
+    uint64_t entry[1];
+};
+
+struct pud {
+    uint64_t entry[1];
+};
+
+struct pgd {
+    uint64_t entry[1];
+};
+
+struct pgprot {
+    uint64_t value;
+} pgprot_t;
+
+/*
+ * Hardware page table definitions.
+ *
+ * Level 0 descriptor (PGD).
+ */
+#define PGD_TYPE_TABLE      (_AC(3, UL) << 0)
+#define PGD_TABLE_BIT		(_AC(1, UL) << 1)
+#define PGD_TYPE_MASK		(_AC(3, UL) << 0)
+#define PGD_TYPE_SECT		(_AC(1, UL) << 0)
+
+/*
+ *
+ * Level 1 descriptor (PUD).
+ */
+#define PUD_TYPE_TABLE      (_AC(3, UL) << 0)
+#define PUD_TABLE_BIT       (_AC(1, UL) << 1)
+#define PUD_TYPE_MASK       (_AC(3, UL) << 0)
+#define PUD_TYPE_SECT       (_AC(1, UL) << 0)
+
+/*
+ * Level 2 descriptor (PMD).
+ */
+#define PMD_TYPE_MASK       (_AC(3, UL) << 0)
+#define PMD_TYPE_FAULT      (_AC(0, UL) << 0)
+#define PMD_TYPE_TABLE      (_AC(3, UL) << 0)
+#define PMD_TYPE_SECT       (_AC(1, UL) << 0)
+#define PMD_TABLE_BIT       (_AC(1, UL) << 1)
+
+/*
+ * Section
+ */
+#define PMD_SECT_VALID      (_AC(1, UL) << 0)
+#define PMD_SECT_USER       (_AC(1, UL) << 6)     /* AP[1] */
+#define PMD_SECT_RDONLY     (_AC(1, UL) << 7)     /* AP[2] */
+#define PMD_SECT_S          (_AC(3, UL) << 8)
+#define PMD_SECT_AF         (_AC(1, UL) << 10)
+#define PMD_SECT_NG         (_AC(1, UL) << 11)
+#define PMD_SECT_CONT       (_AC(1, UL) << 52)
+#define PMD_SECT_PXN        (_AC(1, UL) << 53)
+#define PMD_SECT_UXN        (_AC(1, UL) << 54)
+
+/*
+ * AttrIndx[2:0] encoding (mapping attributes defined in the MAIR* registers).
+ */
+#define PMD_ATTRINDX(t)     (_AC(t, UL) << 2)
+#define PMD_ATTRINDX_MASK   (_AC(7) << 2)
+
+/*
+ * Level 3 descriptor (PTE).
+ */
+#define PTE_TYPE_MASK       (_AC(3, UL) << 0)
+#define PTE_TYPE_FAULT      (_AC(0, UL) << 0)
+#define PTE_TYPE_PAGE       (_AC(3, UL) << 0)
+#define PTE_TABLE_BIT       (_AC(1, UL) << 1)
+#define PTE_USER            (_AC(1, UL) << 6)     /* AP[1] */
+#define PTE_RDONLY          (_AC(1, UL) << 7)     /* AP[2] */
+#define PTE_SHARED          (_AC(3, UL) << 8)     /* SH[1:0], inner shareable */
+#define PTE_AF              (_AC(1, UL) << 10)    /* Access Flag */
+#define PTE_NG              (_AC(1, UL) << 11)    /* nG */
+#define PTE_DBM             (_AC(1, UL) << 51)    /* Dirty Bit Management */
+#define PTE_CONT            (_AC(1, UL) << 52)    /* Contiguous range */
+#define PTE_PXN             (_AC(1, UL) << 53)    /* Privileged XN */
+#define PTE_UXN             (_AC(1, UL) << 54)    /* User XN */
+#define PTE_HYP_XN          (_AC(1, UL) << 54)    /* HYP XN */
+
+#define PTE_WRITE           (PTE_DBM)        /* same as DBM (51) */
+#define PTE_DIRTY           (_AC(1, UL) << 55)
+
+/*
+ * AttrIndx[2:0] encoding (mapping attributes defined in the MAIR* registers).
+ */
+#define PTE_ATTRINDX(t)     (_AC(t, UL) << 2)
+#define PTE_ATTRINDX_MASK   (_AC(7, UL) << 2)
+
+#define PROT_DEFAULT        (PTE_TYPE_PAGE | PTE_AF | PTE_SHARED)
+#define PROT_SECT_DEFAULT   (PMD_TYPE_SECT | PMD_SECT_AF | PMD_SECT_S)
+
+#define PROT_DEVICE_nGnRnE  (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_DEVICE_nGnRnE))
+#define PROT_DEVICE_nGnRE   (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_DEVICE_nGnRE))
+#define PROT_NORMAL_NC      (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_NORMAL_NC))
+#define PROT_NORMAL_WT      (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_NORMAL_WT))
+#define PROT_NORMAL         (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_NORMAL))
+
+#define PROT_SECT_DEVICE_nGnRE  (PROT_SECT_DEFAULT | PMD_SECT_PXN | PMD_SECT_UXN | PMD_ATTRINDX(MT_DEVICE_nGnRE))
+#define PROT_SECT_NORMAL        (PROT_SECT_DEFAULT | PMD_SECT_PXN | PMD_SECT_UXN | PMD_ATTRINDX(MT_NORMAL))
+#define PROT_SECT_NORMAL_EXEC   (PROT_SECT_DEFAULT | PMD_SECT_UXN | PMD_ATTRINDX(MT_NORMAL))
+
+uint64_t ukvm_end_of_kernel_rodata;
+uint64_t ukvm_end_of_kernel_etext;
+
+#endif /* UKVM_CPU_AARCH64_H */
